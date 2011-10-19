8d27d1c4c4bcc0ae679ab894013d98740f899a72
metamn <bartus.csongor@gmail.com>
2011-10-19 20:43:50 +0300

    Article toggle excerpt/content done

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -3,4 +3,8 @@ jQuery.noConflict();
 // Use jQuery via jQuery(...)
 jQuery(document).ready(function(){
   
+  // Startpage. Show article body on click
+  jQuery(".startpage #read-more").click(function() {
+    jQuery(".startpage #body").toggle('slow');
+  });
 });
