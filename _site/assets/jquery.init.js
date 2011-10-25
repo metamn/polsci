6a5d33cc255178a11eaf471d5c274debc7138fc2
metamn <bartus.csongor@gmail.com>
2011-10-25 15:16:20 +0300

    Animation added to main menu

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -3,6 +3,18 @@ jQuery.noConflict();
 // Use jQuery via jQuery(...)
 jQuery(document).ready(function(){
   
+  
+  // Enlarge menu on hover 
+  jQuery("#menu ul li").hover(
+    function () {
+      jQuery(this).addClass('highlight', 300);
+    }, 
+    function () {
+      jQuery(this).removeClass('highlight', 300);
+    }
+  );
+
+  
   // Startpage. Show article body on click
   jQuery(".startpage article").click(function() {
     jQuery(".startpage #body").toggle('slow');   
