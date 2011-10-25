fb34a80b98b815f3784a9c8940fc2cb40a2ca708
metamn <bartus.csongor@gmail.com>
2011-10-25 20:18:05 +0300

    News, Events added

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -4,8 +4,39 @@ jQuery.noConflict();
 jQuery(document).ready(function(){
   
   
+  // Show Events, News, Search ...
+  jQuery("#secondary li h4").click(function() {
+    var id = "#" + jQuery(this).attr('class'); 
+    jQuery(id).toggle('slow', function() {
+      if (jQuery(".startpage #body").is(":visible")) {
+        toggleArticleOpacity("");  
+      } else {
+        toggleArticleOpacity(" #excerpt");  
+      }            
+    });
+      
+  });
+  
+  // Hiding or showing the main article 
+  function toggleArticleOpacity(div) {    
+    var switchOff = false;
+    
+    jQuery.each(["#news", "#events"], function(index, value) { 
+      switchOff = switchOff || (jQuery(".startpage " + value).css('display') ==  'block');
+    });
+    
+    if (switchOff) {
+      jQuery(".startpage article" + div).fadeTo('slow', .1);
+    } else {
+      jQuery(".startpage article" + div).fadeTo('slow', .9);
+    }
+  }
+  
+  
+  
+  
   // Enlarge menu on hover 
-  jQuery("#menu ul li").hover(
+  jQuery("#menu ul li a").hover(
     function () {
       jQuery(this).addClass('highlight', 300);
     }, 
